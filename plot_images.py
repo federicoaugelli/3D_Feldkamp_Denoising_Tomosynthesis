70d13b911e8f7a1dfb8b55a377e017874df888a3
FRDRK <federicoaugelli@gmail.com>
2025-05-26 17:33:43 +0200

    updates

---
+++ plot_images.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+def plot_image(image, title=None, cmap='gray'):
+    """
+    Plot a single image with a title.
+    """
+    plt.figure(figsize=(10, 5))
+    plt.imshow(image, cmap=cmap)
+    if title:
+        plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+# plot multiple images in grid
+def plot_n_images(images, title=None, n=3, cmap='gray'):
+    """
+    Plot n images in a grid.
+    """
+    n = min(n, len(images))
+    fig, axes = plt.subplots(1, n, figsize=(15, 5))
+    for i in range(n):
+        axes[i].imshow(images[i], cmap=cmap)
+        axes[i].axis('off')
+    if title:
+        plt.suptitle(title)
+    plt.show()
